b1da293fecbb98fda7a62ede099153c442ba2a21
akmalu54 <akmaludin4127@gmail.com>
2025-12-27 07:08:01 +0800

    refactoring adalah membuat code kita menjadi lebih sederhana

---
+++ 05-function/refactoring.js
@@ -0,0 +1,9 @@
+function jumlahvolumeduakubus (a,b) {
+    total = a * a * a + b * b * b;;
+
+    return total;
+}
+
+alert(jumlahvolumeduakubus (8,3));
+alert(jumlahvolumeduakubus (5,6));
+alert(jumlahvolumeduakubus (1,9));
