437fc69cb7cb2727afee190c4627559e7760e829
akmalu54 <akmaludin4127@gmail.com>
2025-12-28 21:11:14 +0800

    menambahkan materi array method (foreach, map, sort, filter, dan find

--- 06-array/array.js
+++ 06-array/array.js
@@ -32,5 +32,55 @@ buah.splice(1, 0, "Melon", "Kiwi"); //menyisipkan elemen pada index ke 1, mengha
 var buah2 = ["Mangga", "Apel", "Jeruk", "Pisang", "Semangka"];
 var buah3 = buah2.slice(1, 4); //mengambil elemen dari index 1 sampai index 4 (tidak termasuk index 4) 
 
-console.log(buah.join(" - ")); //menampilkan isi array buah
-console.log(buah3.join(" - ")); //menggabungkan isi array menjadi string dengan pemisah " - "
+// console.log(buah.join(" - ")); //menampilkan isi array buah
+// console.log(buah3.join(" - ")); //menggabungkan isi array menjadi string dengan pemisah " - "
+
+
+// 6. forEach untuk melakukan perulangan pada array
+var angka = [1, 2, 3, 4, 5];
+var mahasiswa = ["Andi", "Budi", "Cici"];
+// for (var i = 0; i < angka.length; i++) {
+//     console.log (angka[i]);
+// }
+
+// angka.forEach(function(e) {
+//     console.log(e);
+// });
+
+
+// mahasiswa.forEach(function(e , i) {
+//     console.log("mahasiswa ke-" + (i +1) + " adalah : " + e);
+// })
+
+// 7. map mengubah isi array dan menghasilkan array baru
+// var angka2 = [1, 2, 3, 7, 5, 8, 4];
+// var angka3 = angka2.map(function(e) {
+//     return e * 2;
+// });
+// console.log(angka3.join(" - "));
+
+// 8. sort mengurutkan isi array
+// var angka4 = [1, 5, 20,67,10, 8, 3, 4, 7, 6];
+// console.log(angka4.join(" - "));
+// angka4.sort(function(a,b) {
+//     return a-b;
+// });
+// console.log( angka4.join(" - "));
+
+
+// 9. filter menyaring isi array berdasarkan kondisi tertentu
+var angka5 = [1, 5, 20, 67, 10, 8, 3, 4, 7, 6];
+var angka6 = angka5.filter(function(x) {
+    return x <= 5;
+});
+console.log(angka6.join(" - "));
+
+
+// 10. find mencari satu elemen pada array berdasarkan kondisi tertentu
+var angka7 = [1, 5, 20, 67, 10, 8, 3, 4, 7, 6];
+var angka8 = angka7.find(function(x) {
+    return x > 5;
+});
+console.log(angka8);
+
+
